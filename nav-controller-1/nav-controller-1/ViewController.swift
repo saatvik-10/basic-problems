209cdaf213b1d9b8ab80478eae938eb2afb9c75e
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-03 11:36:40 +0530

    segments

---
+++ nav-controller-1/nav-controller-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nav-controller-1
+//
+//  Created by SDC-USER on 03/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
