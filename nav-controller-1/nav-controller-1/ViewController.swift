ffd22a8423d686099bdec1bbe7c3892a893fc0e2
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-03 12:13:45 +0530

    segue

--- nav-controller-1/nav-controller-1/ViewController.swift
+++ nav-controller-1/nav-controller-1/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchOutlet: UISwitch!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
+    
+    @IBAction func yellowIdentifier(_ sender: Any) {
+        if switchOutlet.isOn {
+            performSegue(withIdentifier:
+                            "yellow", sender: nil)
+        }
+    }
+    
+    @IBAction func greenIdentifier(_ sender: Any) {
+        if switchOutlet.isOn {
+            performSegue(withIdentifier:
+                            "green", sender: nil)
+        }
+    }
 }
 
