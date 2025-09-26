132570033fd277272ee6170f6f7aa3bd9872cc17
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-26 12:46:48 +0530

    scroll-view

---
+++ scroll-view/scroll-view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scroll-view
+//
+//  Created by SDC-USER on 26/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
